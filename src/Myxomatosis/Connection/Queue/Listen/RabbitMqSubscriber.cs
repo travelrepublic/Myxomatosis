53100909e350c61ed7b18fa2150638fff31750dd
Lawrence Taylor <lawrence.taylor@outlook.com>
2015-03-13 15:30:05 +0000

    Reconnect on EndOfStreamException

--- src/Myxomatosis/Connection/Queue/Listen/RabbitMqSubscriber.cs
+++ src/Myxomatosis/Connection/Queue/Listen/RabbitMqSubscriber.cs
@@ -4,6 +4,7 @@ using Myxomatosis.Logging;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -45,72 +46,82 @@ namespace Myxomatosis.Connection.Queue.Listen
             }
         }
 
-        private void SubscribeToQueue(QueueSubscription subscription, TaskCompletionSource<object> taskCompletionSource)
+        #endregion IRabbitMqSubscriber Members
+
+        private void ListenToQueue(QueueSubscription subscription)
         {
             using (var connection = _connectionFactory.CreateConnection())
             {
                 using (var model = connection.CreateModel())
                 {
-                    try
+                    var consumer = new QueueingBasicConsumer(model);
+                    model.ConfirmSelect(); //try publisher confirms
+
+                    var queueName = subscription.SubscriptionData.Queue;
+
+                    model.DeclareQueue(queueName);
+
+                    model.BasicQos(0, subscription.SubscriptionData.PrefetchCount, false);
+                    model.BasicConsume(queueName, false, consumer);
+                    subscription.OpenEvent.Set();
+
+                    _logger.LogTrace("Subscription set up with Channel Hash {0}", model.GetHashCode());
+
+                    while (subscription.KeepListening)
                     {
-                        var consumer = new QueueingBasicConsumer(model);
-                        model.ConfirmSelect(); //try publisher confirms
+                        BasicDeliverEventArgs basicDeliverEventArgs;
+                        if (!consumer.Queue.Dequeue((int) TimeSpan.FromSeconds(5).TotalMilliseconds, out basicDeliverEventArgs))
+                            continue;
 
-                        var queueName = subscription.SubscriptionData.Queue;
-
-                        model.DeclareQueue(queueName);
-
-                        model.BasicQos(0, subscription.SubscriptionData.PrefetchCount, false); //TODO: make number of concurrent messages retired configurable
-                        model.BasicConsume(queueName, false, consumer);
-                        subscription.OpenEvent.Set();
-
-                        _logger.LogTrace("Subscription set up with Channel Hash {0}", model.GetHashCode());
-
-                        while (subscription.KeepListening)
+                        _logger.LogTrace("Dequeued message with delivery tag {0}", basicDeliverEventArgs.DeliveryTag);
+                        var body = basicDeliverEventArgs.Body;
+                        var headers = basicDeliverEventArgs.BasicProperties.Headers;
+                        var message = new RabbitMessage
                         {
-                            BasicDeliverEventArgs basicDeliverEventArgs;
-                            if (!consumer.Queue.Dequeue((int)TimeSpan.FromSeconds(5).TotalMilliseconds, out basicDeliverEventArgs))
-                                continue;
-
-                            _logger.LogTrace("Dequeued message with delivery tag {0}", basicDeliverEventArgs.DeliveryTag);
-                            var body = basicDeliverEventArgs.Body;
-                            var headers = basicDeliverEventArgs.BasicProperties.Headers;
-                            var message = new RabbitMessage
-                            {
-                                RawMessage = body,
-                                RawHeaders = headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value as byte[]),
-                                Channel = model,
-                                DeliveryTag = basicDeliverEventArgs.DeliveryTag,
-                                ErrorHandler = _errorHandler
-                            };
-                            try
-                            {
-                                subscription.MessageObserver.OnNext(message);
-                            }
-                            catch (Exception e)
-                            {
-                                _errorHandler.Error(message, e);
-                                subscription.MessageObserver.OnError(e);
-                            }
+                            RawMessage = body,
+                            RawHeaders = headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value as byte[]),
+                            Channel = model,
+                            DeliveryTag = basicDeliverEventArgs.DeliveryTag,
+                            ErrorHandler = _errorHandler
+                        };
+                        try
+                        {
+                            subscription.MessageObserver.OnNext(message);
+                        }
+                        catch (Exception e)
+                        {
+                            _errorHandler.Error(message, e);
+                            subscription.MessageObserver.OnError(e);
                         }
                     }
-                    catch (Exception e)
-                    {
-                        _logger.LogError("Error in queue subscriber", e);
-                        taskCompletionSource.SetException(e);
-                        return;
-                    }
-                    finally
-                    {
-                        subscription.MessageObserver.OnCompleted();
-                    }
-
-                    taskCompletionSource.SetResult(null);
                 }
             }
         }
 
-        #endregion IRabbitMqSubscriber Members
+        private void SubscribeToQueue(QueueSubscription subscription, TaskCompletionSource<object> taskCompletionSource)
+        {
+            try
+            {
+                ListenToQueue(subscription);
+            }
+            catch (EndOfStreamException eosE)
+            {
+                SubscribeToQueue(subscription, taskCompletionSource);
+                _logger.LogWarn("EndOfStreamException caught.  Will try to reconnect...");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Error in queue subscriber", e);
+                taskCompletionSource.SetException(e);
+                return;
+            }
+            finally
+            {
+                subscription.MessageObserver.OnCompleted();
+            }
+
+            taskCompletionSource.SetResult(null);
+        }
     }
 
     public enum TaskCompletion
